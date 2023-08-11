5015303e5a94f44bdda305bbe9e695126ee06ccc
fredboe <fredboe@outlook.com>
2023-08-11 10:17:41 +0200

    fixed the test

--- tests/state.rs
+++ tests/state.rs
@@ -6,18 +6,18 @@ mod tests {
     #[test]
     fn test_min_max_point_on_track() {
         let mut track = Cartesian2DTrack::new();
-        track.push(Cartesian2D(0.0, 0.0));
-        track.push(Cartesian2D(-1.0, 0.0));
-        track.push(Cartesian2D(0.0, 1.0));
-        track.push(Cartesian2D(4.0, 5.0));
-        track.push(Cartesian2D(0.0, -3.0));
+        track.push(Cartesian2D::new(0.0, 0.0));
+        track.push(Cartesian2D::new(-1.0, 0.0));
+        track.push(Cartesian2D::new(0.0, 1.0));
+        track.push(Cartesian2D::new(4.0, 5.0));
+        track.push(Cartesian2D::new(0.0, -3.0));
 
         let min_point = min_point_track(&mut track).unwrap();
-        assert_eq!(min_point.0, -1.0);
-        assert_eq!(min_point.1, -3.0);
+        assert_eq!(min_point.x, -1.0);
+        assert_eq!(min_point.y, -3.0);
 
         let max_point = max_point_track(&mut track).unwrap();
-        assert_eq!(max_point.0, 4.0);
-        assert_eq!(max_point.1, 5.0);
+        assert_eq!(max_point.x, 4.0);
+        assert_eq!(max_point.y, 5.0);
     }
 }
